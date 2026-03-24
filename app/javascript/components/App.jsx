ebe7635dda957b230f23501352841fc7b5b70348
Shyam <shyam.javiya@aspireedge.com>
2026-03-24 20:39:49 +0530

    Initial fixes v1.1

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div>
-        <h1>Hello from React Application!!</h1>
+        <h1>Hello from React Application!</h1>
     </div>
   )
 }
